4b4f20aad1fcbb94d4962c310d26568d1ac0b028
shadrack-star <shaddkiplangat98@gmail.com>
2024-03-21 09:08:33 +0300

    creating objects

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St'
+    
+    
+        
+    
+  };
+  const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {
+      ...employee,
+      [key]: value
+    };
+  };
+  
+ // const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'city', 'New York');
+  
+  //console.log(updatedEmployee);
+  const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+  };
+  const deleteFromEmployeeByKey = (employee, key) => {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  };
+  const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+  };
+  
+
